c1ceecaf84c7e501a237db943a0a08ca183fe56b
AmirALi <fhamirali724@gmail.com>
2026-09-21 06:12:32 +0330

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,5 @@
 -- A Advanced Quest System Script For ESX_Legacy (ES_EXTENDED) 
--- Official Repo : https://github.com/AmiraliJahanbakhshRad/AR_Quest
+-- Official Repo : https://github.com/AmirAliZzZ/AR_Quest
 -- if you need change language editing configlang.lua
 -- Redesigned
 -- if you need developer send me msg on discord my id steveplayamir
@@ -8,7 +8,7 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'AmirAliJahanBakhsh'
+author 'AmirAliZzZ'
 description 'AR_Quest - Quest System'
 version '1.0.0'
 
